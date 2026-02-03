8123b1fd7c869b411d6f7642f82001f6aa19fb67
yuvsingh17 <singyuvraj3@gmail.com>
2026-02-03 23:51:44 +0530

    fix mongo env config

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,15 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
+# Load .env only for local development
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URL"))
+MONGO_URL = os.getenv("MONGO_URL")
+
+if not MONGO_URL:
+    raise ValueError("MONGO_URL environment variable not set")
+
+client = MongoClient(MONGO_URL)
 db = client["hrms"]
 
 employees_collection = db["employees"]
